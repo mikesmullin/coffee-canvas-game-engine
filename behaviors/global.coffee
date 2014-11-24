03320ff983df736363c32e5a462e5e2ad058d752
Mike <mike@smullindesign.com>
2014-11-23 22:44:29 -0700

    publish updates

---
+++ behaviors/global.coffee
@@ -0,0 +1,460 @@
+Math.rand = (m,x) -> Math.round(Math.random() * (x-m)) + m
+delay = (s, f) -> setTimeout f, s
+mapRoot = '/models/map1'
+map = 'map1.gltf'
+objects = {}
+
+class VideoSettings
+  @fps: 24 # TODO: find out why mathematically using 60 here lowers it to 10 actual fps
+
+class Time
+  @now: -> (new Date()).getTime()
+
+class Video
+  @canvas: document.getElementById 'mainCanvas'
+  @ctx: @canvas.getContext '2d'
+  @pixelBuf: undefined
+  @drawPixel: (x, y, r, g, b, a) ->
+    index = (x + y * @canvas.width) * 4
+    @pixelBuf.data[index + 0] = r
+    @pixelBuf.data[index + 1] = g
+    @pixelBuf.data[index + 2] = b
+    @pixelBuf.data[index + 3] = a
+  @updateCanvas: ->
+    @ctx.putImageData @pixelBuf, 0, 0
+
+class Engine
+  @running: true
+  @run: ->
+    console.log 'Starting at '+(new Date())
+    @startup =>
+      updateInterval = 1000/VideoSettings.fps
+      maxUpdateLatency = updateInterval * 1
+      drawInterval   = 1000/VideoSettings.fps # should be >= update interval
+      skippedFrames  = 1
+      maxSkipFrames  = 5
+      nextUpdate     = Time.now()
+      framesRendered = 0
+      setInterval (-> console.log "#{framesRendered}fps"; framesRendered = 0), 1000
+
+      #ticks = 0
+      tick = =>
+        #return if ticks++ > 7
+        next = => requestAnimationFrame tick if @running # loop no more than 60fps
+        now = Time.now()
+
+        # if the time to update + draw has left the nextUpdate scheduled
+        # ridiculously far behind in the past, then resync the period for
+        # the nextUpdate to happen immediately, and then resume at intervals from there
+        # otherwise a string of potentially long updates would get called in rapid succession
+        nextUpdate = now if nextUpdate - maxUpdateLatency > now # resync
+        # TODO: this can be expanded to signal unacceptable timing
+        #   and to provide an opportunity to skip some non-essential updates
+        #   in order to catch up faster
+
+        if now >= nextUpdate # past-due for an update
+          nextUpdate += updateInterval # schedule next update an interval apart
+          @update()
+
+          # notice that without an update, we won't have anything new to draw.
+
+          # notice some upates can take too long;
+          # here we're allowed to skip drawing a few frames per second,
+          # in order to catch-up.
+          if now > nextUpdate and # if past time for the next scheduled update, and
+            skippedFrames < maxSkipFrames # we can still afford to skip a few frames
+               skippedFrames++ # skip one more frame
+          else # we have time, or we can't afford to skip any more frames
+            @draw() # take time to draw
+            framesRendered++ # for measuring actual fps
+            skippedFrames = 0 # frames may be skipped from here, if needed
+        else
+          sleepTime = nextUpdate - now
+          if sleepTime > 0
+            return delay sleepTime, next
+
+        next()
+      tick()
+
+  @stop: ->
+  @startup: (cb) ->
+    #focused = false
+    step = 10
+    document.addEventListener 'mousedown', ((e) ->
+      focused = e.target is Video.canvas
+      #return unless focused
+      #console.log 'buttons: ', e.buttons
+    ), true
+    document.addEventListener 'keydown', ((e) ->
+      #return unless focused
+      #console.log 'keyCode: ', e.keyCode
+      switch e.keyCode
+        when 87 # w
+          # TODO: use MonoBehavior style Transform with Vector here
+          objects.player1.yT -= step
+        when 65 # a
+          objects.player1.xT -= step
+        when 83 # s
+          objects.player1.yT += step
+        when 68 # d
+          objects.player1.xT += step
+    ), true
+    initMap map, cb
+  @shutdown: ->
+
+
+  @update: ->
+    # check player collision
+    for name in ['player1', 'player2']
+      obj = objects[name]
+      if obj.xT or obj.yT or obj.zT
+        if collidesWall obj, objects['wall']
+          console.log 'collide'
+        else
+          obj.x += obj.xT
+          obj.y += obj.yT
+          obj.z += obj.zT
+        obj.xT = obj.yT = obj.zT = 0
+
+  @draw: ->
+    #Video.pixelBuf = Video.ctx.createImageData Video.canvas.width, Video.canvas.height
+    #for i in [0...10]
+    #  Video.drawPixel Math.rand(0, Video.canvas.width), Math.rand(0, Video.canvas.height), 255, 0, 0, 255
+    #Video.updateCanvas()
+    Video.ctx.clearRect 0 , 0 , Video.canvas.width, Video.canvas.height
+    drawMap()
+
+collidesWall = (o, wall) ->
+  for nil, i in o.vertices by 3
+    oT = [{
+      x: o.vertices[i].x + o.x + o.xT
+      y: o.vertices[i].y + o.y + o.yT
+    },{
+      x: o.vertices[i+1].x + o.x + o.xT
+      y: o.vertices[i+1].y + o.y + o.yT
+    },{
+      x: o.vertices[i+2].x + o.x + o.xT
+      y: o.vertices[i+2].y + o.y + o.yT
+    }]
+    for nil, ii in wall.vertices by 3
+      wT = [{
+        x: wall.vertices[ii].x
+        y: wall.vertices[ii].y
+      },{
+        x: wall.vertices[ii+1].x
+        y: wall.vertices[ii+1].y
+      },{
+        x: wall.vertices[ii+2].x
+        y: wall.vertices[ii+2].y
+      }]
+      if trianglesIntersect oT, wT
+        return true
+  return false
+
+trianglesIntersect = (a, b) ->
+  # cheating by converting them to rectangles
+  # because they're always at 90 degree angles right now
+  l1x = Math.min a[0].x, a[1].x, a[2].x
+  r1x = Math.max a[0].x, a[1].x, a[2].x
+  l1y = Math.max a[0].y, a[1].y, a[2].y
+  r1y = Math.min a[0].y, a[1].y, a[2].y
+  l2x = Math.min b[0].x, b[1].x, b[2].x
+  r2x = Math.max b[0].x, b[1].x, b[2].x
+  l2y = Math.max b[0].y, b[1].y, b[2].y
+  r2y = Math.min b[0].y, b[1].y, b[2].y
+
+  return false if l1x > r2x || l2x > r1x # aside
+  return false if l1y < r2y || l2y < r1y # above or below
+  return true
+
+
+# 3D Space
+
+#class Vector3
+#  constructor: (@x, @y, @z) ->
+# minimal vector lib
+Vector =
+  UP: x: 0, y: 1, z: 0
+  ZERO: x: 0, y: 0, z: 0
+  dotProduct: (a, b) ->
+    (a.x*b.x)+(a.y*b.y)+(a.z*b.z)
+  crossProduct: (a, b) ->
+    x: (a.y * b.z) - (a.z * b.y)
+    y: (a.z * b.x) - (a.x * b.z)
+    z: (a.x & b.y) - (a.y * b.x)
+  scale: (a, t) ->
+    x: a.x * t
+    y: a.y * t
+    z: a.z * t
+  unitVector: (a) ->
+    Vector.scale a, 1 / Vector.length a
+  add: (a, b) ->
+    x: a.x + b.x
+    y: a.y + b.y
+    z: a.z + b.z
+  add3: (a, b, c) ->
+    x: a.x + b.x + c.x
+    y: a.y + b.y + c.y
+    z: a.z + b.z + c.z
+  subtract: (a, b) ->
+    x: a.x - b.x
+    y: a.y - b.y
+    z: a.z - b.z
+  length: (a) -> # measured by Euclidean norm
+    Math.sqrt Vector.dotProduct a, a
+
+
+
+dotProductVec4 = (a, b) ->
+  #[ # column-major
+  #  (a[0]*b[0])  + (a[1]*b[1])  + (a[2]*b[2])  + (a[3]*b[3]),
+  #  (a[0]*b[4])  + (a[1]*b[5])  + (a[2]*b[6])  + (a[3]*b[7]),
+  #  (a[0]*b[8])  + (a[1]*b[9])  + (a[2]*b[10]) + (a[3]*b[11]),
+  #  (a[0]*b[12]) + (a[1]*b[13]) + (a[2]*b[14]) + (a[3]*b[15])
+  #]
+  [ # row-major
+    (a[0]*b[0]) + (a[1]*b[4]) + (a[2]*b[8])  + (a[3]*b[12]),
+    (a[0]*b[1]) + (a[1]*b[5]) + (a[2]*b[9])  + (a[3]*b[13]),
+    (a[0]*b[2]) + (a[1]*b[6]) + (a[2]*b[10]) + (a[3]*b[14]),
+    (a[0]*b[3]) + (a[1]*b[7]) + (a[2]*b[11]) + (a[3]*b[15])
+  ]
+
+
+
+class Transform
+  #position: new Vector3 0, 0, 0
+  #rotation
+  #localPosition
+  #localRotation
+  translate: (translation) ->
+
+
+
+# Objects
+
+class Behavior
+  transform: new Transform()
+
+# Game
+
+class Box extends Behavior
+
+  update: ->
+    transform.translate
+  draw: ->
+    Video.drawPixel Math.rand(0, Video.canvas.width), Math.rand(0, Video.canvas.height), 255, 0, 0, 255
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+getFile = (type, url, cb) ->
+  xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = ->
+    if @readyState is 4 and @status is 200
+      cb @response
+  xhr.open 'GET', url
+  xhr.responseType = type
+  xhr.send()
+
+getAttrVal = (data, accessor_id, cb) ->
+  accessor = data.accessors[accessor_id]
+  bufferView = data.bufferViews[accessor.bufferView]
+  buffer = data.buffers[bufferView.buffer]
+
+  a = (next) ->
+    return next() if buffer.data
+    getFile 'blob', "#{mapRoot}/#{buffer.uri}", (bin) ->
+      if buffer.type is 'arraybuffer'
+        reader = new FileReader
+        reader.addEventListener 'loadend', ->
+          buffer.data = reader.result
+          next()
+        reader.readAsArrayBuffer bin
+
+  b = ->
+    viewSlice = buffer.data.slice bufferView.byteOffset, bufferView.byteOffset + bufferView.byteLength
+    attrSlice = viewSlice.slice accessor.byteOffset, accessor.byteOffset + (accessor.byteStride * accessor.count)
+    switch accessor.type
+      when 'VEC3'
+        cb new Float32Array attrSlice
+
+  a b
+
+recursivelyFindSceneMeshesWithTransforms = (data) ->
+
+
+loadMap = (map, done_cb, cb) ->
+  getFile 'application/json', map, (response) ->
+    data = JSON.parse response
+    flow = new async
+    matrixHierarchy = []
+    for node in data.scenes[data.scene].nodes
+      matrixHierarchy.push data.nodes[node].matrix
+      for child in data.nodes[node].children
+        matrixHierarchy.push data.nodes[child].matrix
+        for id in data.nodes[child].meshes
+          color = ''
+          rgba = data.materials[data.meshes[id].primitives[0].material].instanceTechnique.values?.diffuse
+          color = "rgba(#{Math.ceil 60+(255*rgba[0])}, #{Math.ceil 30+(255*rgba[1])}, #{Math.ceil 0+(255*rgba[2])}, #{Math.round rgba[3], 1})"
+          ((id, h, color) ->
+            flow.serial (next) ->
+              getAttrVal data, data.meshes[id].primitives[0].attributes.POSITION, (vertices) ->
+                cb data.meshes[id].name, h, color, vertices
+                next()
+          )(id, matrixHierarchy.slice(0), color)
+        matrixHierarchy.pop()
+    flow.go (err) ->
+      done_cb()
+
+
+initMap = (map, cb) ->
+  loadMap "#{mapRoot}/#{map}", cb, (name, h, fill_color, vertices) ->
+    # push all vertices into a new game object
+    object =
+      name: name
+      vertices: []
+      fill: fill_color
+      x: null
+      y: null
+      z: null
+      xT: 0
+      yT: 0
+      zT: 0
+      width: null
+      height: null
+      depth: null
+      min: [null,null,null]
+      max: [null,null,null]
+
+    [world, local] = h
+    h = [
+      local
+
+      world
+
+      ## rotate to top orthogonal perspective
+      # this doesn't work perfectly because its rotating around an arbitrary origin
+      # so for now i rotate everything in blender first, instead
+      #[
+      #  1, 0, 0, 0,
+      #  0, Math.cos(90), -1 * Math.sin(90), 0,
+      #  0, Math.sin(90), Math.cos(90), 0,
+      #  0, 0, 0, 1
+      #]
+
+      # flip along x-axis
+      [
+        1, 0, 0, 0
+        0, -1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+      ]
+
+      # and zoom to fit canvas
+      [
+        40, 0, 0, 0
+        0, 40, 0, 0,
+        0, 0, 40, 0,
+        0, 0, 0, 1
+      ]
+
+      # center
+      [
+        1, 0, 0, 0
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        170, 270, 0, 1
+      ]
+
+    ]
+
+    xmin = ymin = zmin = xmax = ymax = zmax = null
+    for nil, i in vertices by 3
+      p = transform h, {
+        x: vertices[i]
+        y: vertices[i+1]
+        z: vertices[i+2]
+      }
+      xmin = Math.min p.x, if null is xmin then p.x else xmin
+      ymin = Math.min p.y, if null is ymin then p.y else ymin
+      zmin = Math.min p.z, if null is zmin then p.z else zmin
+      xmax = Math.max p.x, if null is xmax then p.x else xmax
+      ymax = Math.max p.y, if null is ymax then p.y else ymax
+      zmax = Math.max p.z, if null is zmax then p.z else zmax
+      object.vertices.push p
+
+    object.min = [xmin, ymin, zmin]
+    object.max = [xmax, ymax, zmax]
+    object.width = xmax - xmin
+    object.height = ymax - ymin
+    object.depth = zmax - zmin
+    object.x = 0
+    object.y = 0
+    object.z = 0
+
+    objects[name] = object
+    console.log object
+
+
+transform = (h, p) ->
+  for matrix in h
+    [p.x, p.y, p.z] = dotProductVec4 [p.x, p.y, p.z, 1], matrix
+  return p
+
+drawMap = ->
+  for name, object of objects
+    Video.ctx.lineWidth = 1
+    Video.ctx.strokeStyle = 'rgba(255, 255, 255, .15)'
+
+    # draw triangles
+    p = object.vertices
+    for nil, i in p by 3
+      Video.ctx.fillStyle = object.fill
+      Video.ctx.beginPath()
+      Video.ctx.moveTo p[i].x+object.x, p[i].y+object.y
+      Video.ctx.lineTo p[i+1].x+object.x, p[i+1].y+object.y
+      Video.ctx.lineTo p[i+2].x+object.x, p[i+2].y+object.y
+      Video.ctx.closePath()
+      Video.ctx.fill()
+      Video.ctx.stroke()
+
+
+
+
+
+
+
+
+
+
+
+
+Engine.run()
+
--- behaviors/global.js
+++ behaviors/global.js
@@ -1,4 +1,4 @@
-var Behavior, Box, Engine, Time, Transform, Vector3, Video, VideoSettings, delay,
+var Behavior, Box, Engine, Time, Transform, Vector, Video, VideoSettings, collidesWall, delay, dotProductVec4, drawMap, getAttrVal, getFile, initMap, loadMap, map, mapRoot, objects, recursivelyFindSceneMeshesWithTransforms, transform, trianglesIntersect,
   __hasProp = {}.hasOwnProperty,
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
@@ -9,6 +9,12 @@ Math.rand = function(m, x) {
 delay = function(s, f) {
   return setTimeout(f, s);
 };
+
+mapRoot = '/models/map1';
+
+map = 'map1.gltf';
+
+objects = {};
 
 VideoSettings = (function() {
   function VideoSettings() {}
@@ -62,89 +68,235 @@ Engine = (function() {
   Engine.running = true;
 
   Engine.run = function() {
-    var drawInterval, framesRendered, maxSkipFrames, maxUpdateLatency, nextUpdate, skippedFrames, tick, updateInterval;
     console.log('Starting at ' + (new Date()));
-    updateInterval = 1000 / VideoSettings.fps;
-    maxUpdateLatency = updateInterval * 1;
-    drawInterval = 1000 / VideoSettings.fps;
-    skippedFrames = 1;
-    maxSkipFrames = 5;
-    nextUpdate = Time.now();
-    framesRendered = 0;
-    setInterval((function() {
-      console.log("" + framesRendered + "fps");
-      return framesRendered = 0;
-    }), 1000);
-    tick = (function(_this) {
+    return this.startup((function(_this) {
       return function() {
-        var next, now, sleepTime;
-        next = function() {
-          if (_this.running) {
-            return requestAnimationFrame(tick);
+        var drawInterval, framesRendered, maxSkipFrames, maxUpdateLatency, nextUpdate, skippedFrames, tick, updateInterval;
+        updateInterval = 1000 / VideoSettings.fps;
+        maxUpdateLatency = updateInterval * 1;
+        drawInterval = 1000 / VideoSettings.fps;
+        skippedFrames = 1;
+        maxSkipFrames = 5;
+        nextUpdate = Time.now();
+        framesRendered = 0;
+        setInterval((function() {
+          console.log("" + framesRendered + "fps");
+          return framesRendered = 0;
+        }), 1000);
+        tick = function() {
+          var next, now, sleepTime;
+          next = function() {
+            if (_this.running) {
+              return requestAnimationFrame(tick);
+            }
+          };
+          now = Time.now();
+          if (nextUpdate - maxUpdateLatency > now) {
+            nextUpdate = now;
           }
-        };
-        now = Time.now();
-        if (nextUpdate - maxUpdateLatency > now) {
-          nextUpdate = now;
-        }
-        if (now >= nextUpdate) {
-          nextUpdate += updateInterval;
-          _this.update();
-          if (now > nextUpdate && skippedFrames < maxSkipFrames) {
-            skippedFrames++;
+          if (now >= nextUpdate) {
+            nextUpdate += updateInterval;
+            _this.update();
+            if (now > nextUpdate && skippedFrames < maxSkipFrames) {
+              skippedFrames++;
+            } else {
+              _this.draw();
+              framesRendered++;
+              skippedFrames = 0;
+            }
           } else {
-            _this.draw();
-            framesRendered++;
-            skippedFrames = 0;
+            sleepTime = nextUpdate - now;
+            if (sleepTime > 0) {
+              return delay(sleepTime, next);
+            }
           }
-        } else {
-          sleepTime = nextUpdate - now;
-          if (sleepTime > 0) {
-            return delay(sleepTime, next);
-          }
-        }
-        return next();
+          return next();
+        };
+        return tick();
       };
-    })(this);
-    return tick();
+    })(this));
   };
 
   Engine.stop = function() {};
 
-  Engine.startup = function() {};
+  Engine.startup = function(cb) {
+    var step;
+    step = 10;
+    document.addEventListener('mousedown', (function(e) {
+      var focused;
+      return focused = e.target === Video.canvas;
+    }), true);
+    document.addEventListener('keydown', (function(e) {
+      switch (e.keyCode) {
+        case 87:
+          return objects.player1.yT -= step;
+        case 65:
+          return objects.player1.xT -= step;
+        case 83:
+          return objects.player1.yT += step;
+        case 68:
+          return objects.player1.xT += step;
+      }
+    }), true);
+    return initMap(map, cb);
+  };
 
   Engine.shutdown = function() {};
 
-  Engine.update = function() {};
+  Engine.update = function() {
+    var name, obj, _i, _len, _ref, _results;
+    _ref = ['player1', 'player2'];
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      name = _ref[_i];
+      obj = objects[name];
+      if (obj.xT || obj.yT || obj.zT) {
+        if (collidesWall(obj, objects['wall'])) {
+          console.log('collide');
+        } else {
+          obj.x += obj.xT;
+          obj.y += obj.yT;
+          obj.z += obj.zT;
+        }
+        _results.push(obj.xT = obj.yT = obj.zT = 0);
+      } else {
+        _results.push(void 0);
+      }
+    }
+    return _results;
+  };
 
   Engine.draw = function() {
-    var i, _i;
-    Video.pixelBuf = Video.ctx.createImageData(Video.canvas.width, Video.canvas.height);
-    for (i = _i = 0; _i < 10; i = ++_i) {
-      Video.drawPixel(Math.rand(0, Video.canvas.width), Math.rand(0, Video.canvas.height), 255, 0, 0, 255);
-    }
-    return Video.updateCanvas();
+    Video.ctx.clearRect(0, 0, Video.canvas.width, Video.canvas.height);
+    return drawMap();
   };
 
   return Engine;
 
 })();
 
-Vector3 = (function() {
-  function Vector3(x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
+collidesWall = function(o, wall) {
+  var i, ii, nil, oT, wT, _i, _j, _len, _len1, _ref, _ref1;
+  _ref = o.vertices;
+  for (i = _i = 0, _len = _ref.length; _i < _len; i = _i += 3) {
+    nil = _ref[i];
+    oT = [
+      {
+        x: o.vertices[i].x + o.x + o.xT,
+        y: o.vertices[i].y + o.y + o.yT
+      }, {
+        x: o.vertices[i + 1].x + o.x + o.xT,
+        y: o.vertices[i + 1].y + o.y + o.yT
+      }, {
+        x: o.vertices[i + 2].x + o.x + o.xT,
+        y: o.vertices[i + 2].y + o.y + o.yT
+      }
+    ];
+    _ref1 = wall.vertices;
+    for (ii = _j = 0, _len1 = _ref1.length; _j < _len1; ii = _j += 3) {
+      nil = _ref1[ii];
+      wT = [
+        {
+          x: wall.vertices[ii].x,
+          y: wall.vertices[ii].y
+        }, {
+          x: wall.vertices[ii + 1].x,
+          y: wall.vertices[ii + 1].y
+        }, {
+          x: wall.vertices[ii + 2].x,
+          y: wall.vertices[ii + 2].y
+        }
+      ];
+      if (trianglesIntersect(oT, wT)) {
+        return true;
+      }
+    }
   }
+  return false;
+};
 
-  return Vector3;
+trianglesIntersect = function(a, b) {
+  var l1x, l1y, l2x, l2y, r1x, r1y, r2x, r2y;
+  l1x = Math.min(a[0].x, a[1].x, a[2].x);
+  r1x = Math.max(a[0].x, a[1].x, a[2].x);
+  l1y = Math.max(a[0].y, a[1].y, a[2].y);
+  r1y = Math.min(a[0].y, a[1].y, a[2].y);
+  l2x = Math.min(b[0].x, b[1].x, b[2].x);
+  r2x = Math.max(b[0].x, b[1].x, b[2].x);
+  l2y = Math.max(b[0].y, b[1].y, b[2].y);
+  r2y = Math.min(b[0].y, b[1].y, b[2].y);
+  if (l1x > r2x || l2x > r1x) {
+    return false;
+  }
+  if (l1y < r2y || l2y < r1y) {
+    return false;
+  }
+  return true;
+};
 
-})();
+Vector = {
+  UP: {
+    x: 0,
+    y: 1,
+    z: 0
+  },
+  ZERO: {
+    x: 0,
+    y: 0,
+    z: 0
+  },
+  dotProduct: function(a, b) {
+    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+  },
+  crossProduct: function(a, b) {
+    return {
+      x: (a.y * b.z) - (a.z * b.y),
+      y: (a.z * b.x) - (a.x * b.z),
+      z: (a.x & b.y) - (a.y * b.x)
+    };
+  },
+  scale: function(a, t) {
+    return {
+      x: a.x * t,
+      y: a.y * t,
+      z: a.z * t
+    };
+  },
+  unitVector: function(a) {
+    return Vector.scale(a, 1 / Vector.length(a));
+  },
+  add: function(a, b) {
+    return {
+      x: a.x + b.x,
+      y: a.y + b.y,
+      z: a.z + b.z
+    };
+  },
+  add3: function(a, b, c) {
+    return {
+      x: a.x + b.x + c.x,
+      y: a.y + b.y + c.y,
+      z: a.z + b.z + c.z
+    };
+  },
+  subtract: function(a, b) {
+    return {
+      x: a.x - b.x,
+      y: a.y - b.y,
+      z: a.z - b.z
+    };
+  },
+  length: function(a) {
+    return Math.sqrt(Vector.dotProduct(a, a));
+  }
+};
+
+dotProductVec4 = function(a, b) {
+  return [(a[0] * b[0]) + (a[1] * b[4]) + (a[2] * b[8]) + (a[3] * b[12]), (a[0] * b[1]) + (a[1] * b[5]) + (a[2] * b[9]) + (a[3] * b[13]), (a[0] * b[2]) + (a[1] * b[6]) + (a[2] * b[10]) + (a[3] * b[14]), (a[0] * b[3]) + (a[1] * b[7]) + (a[2] * b[11]) + (a[3] * b[15])];
+};
 
 Transform = (function() {
   function Transform() {}
-
-  Transform.prototype.position = new Vector3(0, 0, 0);
 
   Transform.prototype.translate = function(translation) {};
 
@@ -179,5 +331,179 @@ Box = (function(_super) {
   return Box;
 
 })(Behavior);
+
+getFile = function(type, url, cb) {
+  var xhr;
+  xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      return cb(this.response);
+    }
+  };
+  xhr.open('GET', url);
+  xhr.responseType = type;
+  return xhr.send();
+};
+
+getAttrVal = function(data, accessor_id, cb) {
+  var a, accessor, b, buffer, bufferView;
+  accessor = data.accessors[accessor_id];
+  bufferView = data.bufferViews[accessor.bufferView];
+  buffer = data.buffers[bufferView.buffer];
+  a = function(next) {
+    if (buffer.data) {
+      return next();
+    }
+    return getFile('blob', "" + mapRoot + "/" + buffer.uri, function(bin) {
+      var reader;
+      if (buffer.type === 'arraybuffer') {
+        reader = new FileReader;
+        reader.addEventListener('loadend', function() {
+          buffer.data = reader.result;
+          return next();
+        });
+        return reader.readAsArrayBuffer(bin);
+      }
+    });
+  };
+  b = function() {
+    var attrSlice, viewSlice;
+    viewSlice = buffer.data.slice(bufferView.byteOffset, bufferView.byteOffset + bufferView.byteLength);
+    attrSlice = viewSlice.slice(accessor.byteOffset, accessor.byteOffset + (accessor.byteStride * accessor.count));
+    switch (accessor.type) {
+      case 'VEC3':
+        return cb(new Float32Array(attrSlice));
+    }
+  };
+  return a(b);
+};
+
+recursivelyFindSceneMeshesWithTransforms = function(data) {};
+
+loadMap = function(map, done_cb, cb) {
+  return getFile('application/json', map, function(response) {
+    var child, color, data, flow, id, matrixHierarchy, node, rgba, _fn, _i, _j, _k, _len, _len1, _len2, _ref, _ref1, _ref2, _ref3;
+    data = JSON.parse(response);
+    flow = new async;
+    matrixHierarchy = [];
+    _ref = data.scenes[data.scene].nodes;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      node = _ref[_i];
+      matrixHierarchy.push(data.nodes[node].matrix);
+      _ref1 = data.nodes[node].children;
+      for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+        child = _ref1[_j];
+        matrixHierarchy.push(data.nodes[child].matrix);
+        _ref2 = data.nodes[child].meshes;
+        _fn = function(id, h, color) {
+          return flow.serial(function(next) {
+            return getAttrVal(data, data.meshes[id].primitives[0].attributes.POSITION, function(vertices) {
+              cb(data.meshes[id].name, h, color, vertices);
+              return next();
+            });
+          });
+        };
+        for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
+          id = _ref2[_k];
+          color = '';
+          rgba = (_ref3 = data.materials[data.meshes[id].primitives[0].material].instanceTechnique.values) != null ? _ref3.diffuse : void 0;
+          color = "rgba(" + (Math.ceil(60 + (255 * rgba[0]))) + ", " + (Math.ceil(30 + (255 * rgba[1]))) + ", " + (Math.ceil(0 + (255 * rgba[2]))) + ", " + (Math.round(rgba[3], 1)) + ")";
+          _fn(id, matrixHierarchy.slice(0), color);
+        }
+        matrixHierarchy.pop();
+      }
+    }
+    return flow.go(function(err) {
+      return done_cb();
+    });
+  });
+};
+
+initMap = function(map, cb) {
+  return loadMap("" + mapRoot + "/" + map, cb, function(name, h, fill_color, vertices) {
+    var i, local, nil, object, p, world, xmax, xmin, ymax, ymin, zmax, zmin, _i, _len;
+    object = {
+      name: name,
+      vertices: [],
+      fill: fill_color,
+      x: null,
+      y: null,
+      z: null,
+      xT: 0,
+      yT: 0,
+      zT: 0,
+      width: null,
+      height: null,
+      depth: null,
+      min: [null, null, null],
+      max: [null, null, null]
+    };
+    world = h[0], local = h[1];
+    h = [local, world, [1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1], [40, 0, 0, 0, 0, 40, 0, 0, 0, 0, 40, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 170, 270, 0, 1]];
+    xmin = ymin = zmin = xmax = ymax = zmax = null;
+    for (i = _i = 0, _len = vertices.length; _i < _len; i = _i += 3) {
+      nil = vertices[i];
+      p = transform(h, {
+        x: vertices[i],
+        y: vertices[i + 1],
+        z: vertices[i + 2]
+      });
+      xmin = Math.min(p.x, null === xmin ? p.x : xmin);
+      ymin = Math.min(p.y, null === ymin ? p.y : ymin);
+      zmin = Math.min(p.z, null === zmin ? p.z : zmin);
+      xmax = Math.max(p.x, null === xmax ? p.x : xmax);
+      ymax = Math.max(p.y, null === ymax ? p.y : ymax);
+      zmax = Math.max(p.z, null === zmax ? p.z : zmax);
+      object.vertices.push(p);
+    }
+    object.min = [xmin, ymin, zmin];
+    object.max = [xmax, ymax, zmax];
+    object.width = xmax - xmin;
+    object.height = ymax - ymin;
+    object.depth = zmax - zmin;
+    object.x = 0;
+    object.y = 0;
+    object.z = 0;
+    objects[name] = object;
+    return console.log(object);
+  });
+};
+
+transform = function(h, p) {
+  var matrix, _i, _len, _ref;
+  for (_i = 0, _len = h.length; _i < _len; _i++) {
+    matrix = h[_i];
+    _ref = dotProductVec4([p.x, p.y, p.z, 1], matrix), p.x = _ref[0], p.y = _ref[1], p.z = _ref[2];
+  }
+  return p;
+};
+
+drawMap = function() {
+  var i, name, nil, object, p, _results;
+  _results = [];
+  for (name in objects) {
+    object = objects[name];
+    Video.ctx.lineWidth = 1;
+    Video.ctx.strokeStyle = 'rgba(255, 255, 255, .15)';
+    p = object.vertices;
+    _results.push((function() {
+      var _i, _len, _results1;
+      _results1 = [];
+      for (i = _i = 0, _len = p.length; _i < _len; i = _i += 3) {
+        nil = p[i];
+        Video.ctx.fillStyle = object.fill;
+        Video.ctx.beginPath();
+        Video.ctx.moveTo(p[i].x + object.x, p[i].y + object.y);
+        Video.ctx.lineTo(p[i + 1].x + object.x, p[i + 1].y + object.y);
+        Video.ctx.lineTo(p[i + 2].x + object.x, p[i + 2].y + object.y);
+        Video.ctx.closePath();
+        Video.ctx.fill();
+        _results1.push(Video.ctx.stroke());
+      }
+      return _results1;
+    })());
+  }
+  return _results;
+};
 
 Engine.run();
