8fec7e6b1ce79f25c6447e7b99cc1c6ab6c25829
Mike <mike@smullindesign.com>
2014-11-25 20:36:43 -0700

    make publish a little smoother

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -x
 
-mv src/public/* .
+rsync -avL src/public/* ../ccge-static/
+rm -rf ../ccge-static/behaviors/*.coffee
+rm -rf ../ccge-static/stylesheets/*.styl
 echo done. Remember to update index.html
